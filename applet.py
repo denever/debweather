77470c6938f3a1475f6b88ae6be1d026717d40fa
Giuseppe Martino <denever@tuscio.fi.trl>
2009-11-23 10:23:35 +0100

    Added http retreving Added XML parsing Added periodic update

--- applet.py
+++ applet.py
@@ -30,8 +30,26 @@ pygtk.require('2.0')
 import gtk
 import gnomeapplet
 import gobject
+import httplib
+import logging
+from xml.etree.ElementTree import XML
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='/home/denever/applet.log',
+                    filemode='w')
 
 class WeatherIcon(gtk.Label):
+    description = 'unknown'
+    total = 'unknown'
+    broken = 'unknown'
+    url = 'unknown'
+    weather = 0
+
+    def __init__(self, distro, arch):
+        self.weather_url = "/edos-debcheck/results/%s/latest/%s/weather.xml" % (distro, arch)
+
     def set_clear(self):
         self.set_label("Debian Weather: Clear")
 
@@ -46,17 +64,64 @@ class WeatherIcon(gtk.Label):
 
     def set_storm(self):
         self.set_label("Debian Weather: Storm")
+        
+    def update(self):
+        logging.info("Calling weather update")
+        logging.info(self.weather_url)
+
+        data = str()
+        # try:
+        #     logging.info("Connecting to edos.debian.net...")
+        #     conn = httplib.HTTPConnection("edos.debian.net")
+        #     logging.info("Getting weather_url...")
+        #     conn.request("GET", self.weather_url)
+        #     logging.info("Getting response...")
+        #     r1 = conn.getresponse()
+        #     logging.info(str(r1))
+        #     data = r1.read()
+        #     logging.info(data)
+        # except e:
+        #     logging.error(str(e))
+
+        # try:
+        #     weather_xml = XML(data)
+        #     self.description = weather_xml.getiterator("description")[0].text
+        #     self.weather = weather_xml.getiterator("index")[0].text            
+        #     self.total = weather_xml.getiterator('total')[0].text
+        #     self.broken = weather_xml.getiterator('broken')[0].text
+        #     self.url = weather_xml.getiterator('url')[0].text
+        # except e:
+        #     logging.error(str(e))
+        self.weather = '1'
+
+        logging.info(self.description)
+        logging.info(self.weather)
+        logging.info(self.total)
+        logging.info(self.broken)
+        logging.info(self.url)
+
+        if self.weather == '1':
+            self.set_clear()
+        if self.weather == '2':
+            self.set_few_clouds()
+        if self.weather == '3':
+            self.set_overcast()
+        if self.weather == '4':
+            self.set_shower()
+        if self.weather == '5':
+            self.set_storm()
 
 def background_show(applet):
-    print "background: ", applet.get_background()
+    logging.info("background: %s" % applet.get_background())
 
 def sample_factory(applet, iid):
-    print "Creating new applet instance"
-    weathericon = WeatherIcon()
+    logging.info("Creating new applet instance")
+    weathericon = WeatherIcon('unstable','i386')
     weathericon.set_storm()
     applet.add(weathericon)
     applet.show_all()
     gobject.timeout_add(1000, background_show, applet)
+    gobject.timeout_add(3600000, weathericon.update)
     return True
 
 print "Starting factory"
