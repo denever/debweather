779be2fa99f15141a73bef29e1257b4b0c75e318
Giuseppe Martino <denever@tuscio.fi.trl>
2009-11-26 18:54:29 +0100

    Fixed all bugs in preferencesbox.py

--- debweatherlib/preferencesbox.py
+++ debweatherlib/preferencesbox.py
@@ -1,5 +1,7 @@
+import logging
 import pygtk
 pygtk.require('2.0')
+
 import gtk
 import gconf
 
@@ -13,12 +15,14 @@ class PreferencesBox:
     cmb_distro = None
     cmb_arch = None
 
-    def __init__(self, current_distro, current_arch):
+    def __init__(self, paths, current_distro, current_arch):
         self.gui = gtk.Builder()
-        self.gui.add_from_file(globalconfig.get_in_data_path('prefbox.glade'))
-        self.dialog = gui.get_object('prefs')
-        self.cmb_distro = gui.get_object('cmb_distro')
-        self.cmb_arch = gui.get_object('cmb_arch')
+        self.gui.add_from_file(paths.get_in_data_path('prefbox.glade'))
+        self.dialog = self.gui.get_object('prefs')
+        self.cmb_distro = self.gui.get_object('cmb_distro')
+        self.cmb_arch = self.gui.get_object('cmb_arch')
+        self.cmb_distro.set_model(gtk.ListStore(str))
+        self.cmb_arch.set_model(gtk.ListStore(str))
         self.gui.connect_signals(self)
 
         for i,distro in enumerate(self.distros):
@@ -26,31 +30,31 @@ class PreferencesBox:
             if distro == current_distro:
                 self.cmb_distro.set_active(i)
 
-        for i,arch in enumerate(archs[current_distro]):
+        for i,arch in enumerate(self.archs[current_distro]):
             self.cmb_arch.append_text(arch)
             if arch == current_arch:
-                self.cmb_archs.set_active(i)
+                self.cmb_arch.set_active(i)
 
     def show(self):
         self.dialog.show()
 
-    def on_cmb_distro_changed(self):
+    def on_cmb_distro_changed(self, widget):
         logging.debug("Distro changed")
 
-    def on_cmb_arch_changed(self):
+    def on_cmb_arch_changed(self, widget):
         logging.debug("Arch changed")
 
-    def on_btn_apply(self):
+    def on_btn_apply_clicked(self, widget):
         logging.debug("Apply")
-        distro = self.distro_combo.get_active_text()
-        arch = self.archs_combo.get_active_text()
+        distro = self.cmb_distro.get_active_text()
+        arch = self.cmb_arch.get_active_text()
         conf_client = gconf.client_get_default()
         if conf_client.dir_exists('/apps/pydebweather'):
             conf_client.set_string('/apps/pydebweather/distro', distro)
             conf_client.set_string('/apps/pydebweather/arch', arch)
 
-        self.window.hide()
+        self.dialog.hide()
 
-    def on_btn_cancel(self):
+    def on_btn_cancel_clicked(self, widget):
         logging.debug("Cancel")
         self.dialog.hide()
