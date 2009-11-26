80970f0c28f30e34d777005d475b105f74a9f765
Giuseppe Martino <denever@tuscio.fi.trl>
2009-11-26 20:02:07 +0100

    New showui

--- showui.py
+++ showui.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
-
 import pygtk
+pygtk.require('2.0')
 import gtk
+import logging
+from debweatherlib import Paths
+from debweatherlib import PreferencesBox
 
-gui = gtk.Builder()
-gui.add_from_file('./prefbox.glade')
-prefs = gui.get_object('prefs')
-prefs.show_all()
-gtk.main()
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='/home/denever/work/debweather/showui.log',
+                    filemode='w')
 
+def main():
+    paths = Paths(__file__)
+    pb = PreferencesBox(paths, 'unstable','i386')
+    pb.show()
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
