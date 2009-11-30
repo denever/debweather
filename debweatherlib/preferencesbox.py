56d333bbb923e753020919a5b5db90ed78130f77
Giuseppe Martino <denever@tuscio.fi.trl>
2009-11-30 10:31:53 +0100

    Added emission of signa new-preferences imported GObject Now PreferencesBox is a GObject

--- debweatherlib/preferencesbox.py
+++ debweatherlib/preferencesbox.py
@@ -4,17 +4,24 @@ pygtk.require('2.0')
 
 import gtk
 import gconf
+import gobject
 
-class PreferencesBox:
+class PreferencesBox(gobject.GObject):
+    __gsignals__ = {
+    'new-preferences':(gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_GSTRING, gobject.TYPE_GSTRING,)),
+        }
+    
     archs = {
         'stable':['alpha','amd64','arm','hppa','i386','ia64','mips','mipsel','powerpc','s390','sparc'],
         'testing':['alpha','amd64','arm','armel','hppa','i386','ia64','mips','mipsel','powerpc','s390','sparc'],
         'unstable':['alpha','amd64','arm','armel','hppa','hurd-i386','i386','ia64','m68k','mips','mipsel','powerpc','s390','sparc']}
 
     def __init__(self, paths, current_distro, current_arch):
+        gobject.GObject.__init__(self)
         self.gui = gtk.Builder()
         self.gui.add_from_file(paths.get_in_data_path('prefbox.ui'))
         self.dlg_prefs = self.gui.get_object('dlg_prefs')
+        self.btn_apply = self.gui.get_object('btn_apply')
         self.cmb_distro = self.gui.get_object('cmb_distro')
         self.cmb_arch = self.gui.get_object('cmb_arch')
         self.lst_distros = self.cmb_distro.get_model()
@@ -40,7 +47,6 @@ class PreferencesBox:
             if arch == current_arch:
                 self.cmb_arch.set_active(i)
 
-
     def show(self):
         self.dlg_prefs.show()
 
@@ -64,9 +70,11 @@ class PreferencesBox:
         if conf_client.dir_exists('/apps/pydebweather'):
             conf_client.set_string('/apps/pydebweather/distro', distro)
             conf_client.set_string('/apps/pydebweather/arch', arch)
-
+        self.emit('new-preferences', distro, arch)
         self.dlg_prefs.hide()
 
     def on_btn_cancel_clicked(self, widget):
         logging.debug("Cancel")
         self.dlg_prefs.hide()
+
+gobject.type_register(PreferencesBox)
