77cd7aeaf05f0e7c11c57ed0c369cd5f7c042d3b
Giuseppe Martino <denever@tuscio.fi.trl>
2009-12-04 17:40:32 +0100

    Updated setup.py to the new name of the package

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='debweather',
+setup(name='debian-weather-applet',
       version='1.0',
       description='Debian Weather Applet',
       author=['Giuseppe Martino'],
       author_email=['denever@users.sf.net'],
-      url='http://github.com/denever/debweather',
-      scripts=['debweather'],
+      url='http://github.com/denever/debian-weather-applet',
+      scripts=['debian-weather-applet'],
       packages=['debweatherlib']
      )
